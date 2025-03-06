f685106456cab87424d952573c7b38593b5e3dd1
TienNHM <TienNHM.it@gmail.com>
2025-03-06 23:43:55 +0700

    Animation in RecyclerView

--- app/src/main/java/com/student22110006/baitap04_22110006/ui/MainAnimationActivity.kt
+++ app/src/main/java/com/student22110006/baitap04_22110006/ui/MainAnimationActivity.kt
@@ -1,14 +1,23 @@
 package com.student22110006.baitap04_22110006.ui
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.student22110006.baitap04_22110006.R
+import com.student22110006.baitap04_22110006.data.adapter.CustomAnimationAdapter
+
 
 class MainAnimationActivity : AppCompatActivity() {
+    private lateinit var btnAddItem: Button;
+    private lateinit var rvItems: RecyclerView;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +26,26 @@ class MainAnimationActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        btnAddItem = findViewById<View>(R.id.btn_add_item) as Button
+        rvItems = findViewById<View>(R.id.rv_items) as RecyclerView
+
+        val data: MutableList<String> = ArrayList()
+        for (i in 0..4) {
+            data.add("item $i")
+        }
+
+        val adapter = CustomAnimationAdapter(data)
+        rvItems.adapter = adapter
+        rvItems.layoutManager = LinearLayoutManager(this)
+
+        // set ItemAnimator for RecyclerView
+        rvItems.itemAnimator = DefaultItemAnimator()
+
+        btnAddItem.setOnClickListener {
+            adapter.addItem("new item")
+            rvItems.scrollToPosition(adapter.itemCount - 1)
         }
     }
 }
